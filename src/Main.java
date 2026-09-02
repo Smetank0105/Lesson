13ed24b7a7d202a1d69e97a9c86ff7622bf3d3c1
Smetank0105 <206859408+Smetank0105@users.noreply.github.com>
2026-09-02 20:09:04 +0700

    Lesson_4 4ая задача

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,33 @@
 void main() {
-    //1
-    Scanner console = new Scanner(System.in);
-    System.out.print("Введите свой возраст: ");
-    int age = console.nextInt();
-    boolean result = MyClass.isAdult(age);
-    if(result){
-        System.out.println("Поздравляю, вам доступна скрытая секция \"Алкоголь\"");
-    }else{
-        System.out.println("Вход только со взрослыми.");
-    }
+//    //1
+//    Scanner console = new Scanner(System.in);
+//    System.out.print("Введите свой возраст: ");
+//    int age = console.nextInt();
+//    boolean result = MyClass.isAdult(age);
+//    if(result){
+//        System.out.println("Поздравляю, вам доступна скрытая секция \"Алкоголь\"");
+//    }else{
+//        System.out.println("Вход только со взрослыми.");
+//    }
+//
+//    //2
+//    System.out.print("Введите число: ");
+//    int number = console.nextInt();
+//    result = MyClass.isSimple(number);
+//    if(result){
+//        System.out.println("Число простое.");
+//    }else{
+//        System.out.println("Число составное.");
+//    }
+//
+//    //3
+//    System.out.print("Введите строку: ");
+//    console.nextLine();
+//    String str = console.nextLine();
+//    int count = MyClass.checkVowels(str);
+//    System.out.println("Количество гласных: "+count);
 
-    //2
-    System.out.print("Введите число: ");
-    int number = console.nextInt();
-    result = MyClass.isSimple(number);
-    if(result){
-        System.out.println("Число простое.");
-    }else{
-        System.out.println("Число составное.");
-    }
-
-    //3
-    System.out.print("Введите строку: ");
-    console.nextLine();
-    String str = console.nextLine();
-    int count = MyClass.checkVowels(str);
-    System.out.println("Количество гласных: "+count);
+    //4
+    double result = MyClass.getAvr();
+    System.out.println("Средняя оценка: "+result);
 }
--- src/MyClass.java
+++ src/MyClass.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class MyClass {
     static boolean isAdult(int age){
         return age>18;
@@ -28,5 +30,38 @@ public class MyClass {
             }
         }
         return count;
+    }
+
+    private static int checkStr(String str){
+        String digits = str.replaceAll("\\D+", "");
+        if(digits.isEmpty()){
+            return 0;
+        }
+        int number = Integer.parseInt(digits);
+        if(number >= 2 && number <= 5){
+            return number;
+        }
+        return 0;
+    }
+
+    static double getAvr(){
+        int digits = 0;
+        int count = 0;
+        Scanner console = new Scanner(System.in);
+        String str = "";
+        while(!str.equals("stop")){
+            System.out.print("Введите оценку от 2 до 5 (Для выхода из цикла введите stop): ");
+            str = console.nextLine();
+            int number = checkStr(str);
+            if(number >= 2 && number <= 5){
+                digits += number;
+                count++;
+            }
+        }
+        double result = 0.0;
+        if(count > 0){
+            result = (double)digits/count;
+        }
+        return result;
     }
 }
